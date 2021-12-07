50b86d30aff4dabb748620b8881d31c44df7c1ab
Kshitij Sharma <kshitij.sharma23@gmail.com>
2021-12-07 12:00:08 +0530

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// Mount function to start up the app.
+const mount = (el) => {
+  ReactDOM.render(<App />,el,);
+};
+
+// If in development or isolation
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// Running through container
+// Export mount function
+export { mount };
